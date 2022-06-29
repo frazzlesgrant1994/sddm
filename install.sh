8b40417b8e88d6cb101b662d5e1c7706d852c64b
frazzlesgrant1994 <frazzlesgrant@hotmail.com>
2022-06-29 20:59:09 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+ 
+#! /bin/bash
+
+# Author: Frazer Grant
+# Arch Sddm theme installer
+
+sudo mkdir /usr/share/sddm/themes/ArchLinux
+sudo cp -r components /usr/share/sddm/themes/ArchLinux/
+sudo cp -r faces /usr/share/sddm/themes/ArchLinux/
+sudo cp -r background.png /usr/share/sddm/themes/ArchLinux/
+sudo cp -r Background.qml /usr/share/sddm/themes/ArchLinux/
+sudo cp -r default-logo.svg /usr/share/sddm/themes/ArchLinux/
+sudo cp -r KeyboardButton.qml /usr/share/sddm/themes/ArchLinux/
+sudo cp -r Login.qml /usr/share/sddm/themes/ArchLinux/
+sudo cp -r Main.qml /usr/share/sddm/themes/ArchLinux/
+sudo cp -r metadata.desktop /usr/share/sddm/themes/ArchLinux/
+sudo cp -r preview.png /usr/share/sddm/themes/ArchLinux/
+sudo cp -r SessionButton.qml /usr/share/sddm/themes/ArchLinux/
+sudo cp -r theme.conf /usr/share/sddm/themes/ArchLinux/
